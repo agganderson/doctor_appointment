cd2bdea7d8d565accefa23d2c4d8ed471cd02cbf
Alexandra Anderson <aliganderson@gmail.com>
2016-05-05 11:52:28 -0700

    3 appt min validations

--- server/controllers/appointments.js
+++ server/controllers/appointments.js
@@ -15,17 +15,26 @@ module.exports = (function(){
 			});
 		}, 
 		create: function(req, res){
-			console.log('about to save this ish');
-			var newAppointment = new Appointment({user: req.body.user, complaint: req.body.complaint, time: req.body.time, date: req.body.date});
-			// console.log({complaint: req.body.complaint});
-			newAppointment.save(function(err, results){
-				if(err){
-					console.log("Couldn't save it sorry not sorry", results);
-				}
-				else {
-					console.log('SSAAAAAAAAAAFE');
-					res.json(results);
-				}
+			Appointment.find({date: req.body.date}, function(err, results){
+				Appointment.count({date: req.body.date}, function(err, count){
+					if(count >= 3){
+						res.json({error: "Date chosen is filled, please choose another date."});
+					}
+					else {
+						console.log('about to save this ish');
+						var newAppointment = new Appointment({user: req.body.user, complaint: req.body.complaint, time: req.body.time, date: req.body.date});
+						// console.log({complaint: req.body.complaint});
+						newAppointment.save(function(err, results){
+							if(err){
+								console.log("Couldn't save it sorry not sorry", results);
+							}
+							else {
+								console.log('SSAAAAAAAAAAFE');
+								res.json({error: "Your apppointment is scheduled."});
+							}
+						});
+					}
+				});
 			});
 		},
 		delete: function(req, res){
